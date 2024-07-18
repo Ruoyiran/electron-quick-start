231e028e8b2a6336c987e43c09e78520bb4f584b
Roy Ran <ranpeng@bytedance.com>
2024-07-18 21:03:32 +0800

    hello, world

---
+++ scripts/app.mjs
@@ -0,0 +1,10 @@
+import concurrently from 'concurrently';
+
+concurrently([
+  { command: 'yarn start:dev', name: 'dev' },
+  { command: 'yarn start:server', name: 'server' },
+  {
+    command: 'yarn start:electron',
+    name: 'watch'
+  }
+]);
